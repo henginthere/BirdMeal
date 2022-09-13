c5039e0c1fb70a89e2cdec54ac3a94b696897b33
henginthere <hypae@naver.com>
2022-09-13 14:35:10 +0900

    [S07P22D101-41,S07P22D101-35] [BE] - CHORE : 개발 환경 구축

---
+++ birdmeal/src/main/java/java/backend/birdmeal/BirdmealApplication.java
@@ -0,0 +1,13 @@
+package java.backend.birdmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdmealApplication.class, args);
+	}
+
+}
