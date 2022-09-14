090553c534fab92c11c01c6d3f56bd308a65195c
배혜연 <yeonbb98@naver.com>
2022-09-14 09:55:08 +0900

    [BE] - TEST : commit test

--- birdmeal/src/main/java/com/backend/birdmeal/entity/user/UserEntity.java
+++ birdmeal/src/main/java/com/backend/birdmeal/entity/user/UserEntity.java
@@ -61,7 +61,6 @@ public class UserEntity implements Serializable {
     private String userUpdateDate;
 
 
-
     // 회원 권한
     @ManyToMany // user와 authority 다대다 관계를 일대다, 다대일 관계의 조인테이블로 정의
     @JoinTable(
@@ -69,6 +68,5 @@ public class UserEntity implements Serializable {
             joinColumns = {@JoinColumn(name = "id", referencedColumnName = "user_seq")},
             inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
     private Set<Authority> authorities;
-
 
 }
