9ee868f6a5653e041317cf5f12fdc611af5eafd7
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-14 11:26:31 +0900

    [AOS] - FEAT : 회원가입 틀 구성 (S07P22D101-29)
    화면 레이아웃 제작
    주요 로직 구성

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/login/join/JoinFragment.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/login/join/JoinFragment.kt
@@ -7,7 +7,20 @@ import com.ssafy.birdmeal.databinding.FragmentJoinBinding
 class JoinFragment : BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
 
     override fun init() {
+        initClickListener()
+    }
 
+    private fun initClickListener() = with(binding) {
+
+        // 일반 회원가입
+        containerGeneral.setOnClickListener {
+            // 회원가입 api 호출
+        }
+
+        // 아동 회원가입입
+        containerChild.setOnClickListener {
+            // 카드 인증 페이지 이동
+        }
     }
 
 }
