e9fca10c269bc8cd06dcdc2c3bfe77fc265758da
Kim Ji Soo <us13579@naver.com>
2022-09-15 04:06:44 +0900

    [BE] - FEAT : Seller 판매자 정보 수정 (S07P22D101-100)

--- birdmeal/src/main/java/com/backend/birdmeal/service/SellerInfoService.java
+++ birdmeal/src/main/java/com/backend/birdmeal/service/SellerInfoService.java
@@ -1,6 +1,7 @@
 package com.backend.birdmeal.service;
 
 import com.backend.birdmeal.dto.SellerDto;
+import com.backend.birdmeal.dto.SellerUpdateDto;
 import com.backend.birdmeal.entity.SellerEntity;
 import com.backend.birdmeal.mapper.SellerMapper;
 import com.backend.birdmeal.repository.SellerInfoRepository;
@@ -8,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -38,6 +38,26 @@ public class SellerInfoService {
         SellerEntity sellerEntity = SellerMapper.MAPPER.toEntity(sellerDto);
 
         // 저장하기
+        sellerInfoRepository.save(sellerEntity);
+
+        return true;
+    }
+
+    // 판매자 정보 수정
+    public boolean updateSellerInfo(SellerUpdateDto sellerUpdateDto){
+        // 판매자 정보가 없으면 false
+        if(sellerUpdateDto == null) return false;
+
+        // 먼저 SellerSeq로 Seller 정보 가져오기
+        SellerEntity sellerEntity = sellerInfoRepository.findBySellerSeq(sellerUpdateDto.getSellerSeq());
+
+        // update 해주기
+        sellerEntity.setSellerNickname(sellerUpdateDto.getSellerNickname());
+        sellerEntity.setSellerTel(sellerUpdateDto.getSellerTel());
+        sellerEntity.setSellerAddress(sellerUpdateDto.getSellerAddress());
+        sellerEntity.setSellerInfo(sellerUpdateDto.getSellerInfo());
+
+        // Entity 저장하기
         sellerInfoRepository.save(sellerEntity);
 
         return true;
