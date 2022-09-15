ef37484ae09a37ff49390543891362e6287721b5
Kim Ji Soo <us13579@naver.com>
2022-09-15 10:47:55 +0900

    [BE] - FEAT : Seller 상품 판매 등록 카테고리, 판매자 없는 경우 처리 (S07P22D101-94)

--- birdmeal/src/main/java/com/backend/birdmeal/controller/SellerProductController.java
+++ birdmeal/src/main/java/com/backend/birdmeal/controller/SellerProductController.java
@@ -31,7 +31,28 @@ public class SellerProductController {
     @ApiOperation(value="상품 판매 등록",response = Object.class)
     @PostMapping("")
     public ResponseEntity<?> setSellerProduct(@RequestBody ProductDto productDto){
-        ResponseFrame<?> res = ResponseFrame.of(sellerProductService.setSellerProduct(productDto),"상품 판매 등록을 성공했습니다.");
+        boolean success = sellerProductService.setSellerProduct(productDto);
+        ResponseFrame<?> res;
+
+        if(success) {
+            res = ResponseFrame.of("true", "상품 판매 등록을 성공했습니다.");
+        }else{
+            res = ResponseFrame.of("false", "카테고리 혹은 판매자 정보가 없으므로 상품 판매 등록을 실패했습니다.");
+        }
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
+
+//    /**
+//     * 상품 정보 수정
+//     *
+//     * @param productDto
+//     * @return Object
+//     */
+//
+//    @ApiOperation(value="상품 정보 수정",response = Object.class)
+//    @PostMapping("")
+//    public ResponseEntity<?> updateSellerProduct(@RequestBody ProductDto productDto){
+//        ResponseFrame<?> res = ResponseFrame.of(sellerProductService.setSellerProduct(productDto),"상품 정보 수정을 성공했습니다.");
+//        return new ResponseEntity<>(res, HttpStatus.OK);
+//    }
 }
