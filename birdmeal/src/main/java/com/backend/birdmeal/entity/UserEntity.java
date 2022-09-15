f4b3901fe9ff022a9bf95a3bc4cb354feecf3f89
배혜연 <yeonbb98@naver.com>
2022-09-15 18:01:40 +0900

    [BE] - FEAT : 회원가입 API (S07P22D101-53)

--- birdmeal/src/main/java/com/backend/birdmeal/entity/UserEntity.java
+++ birdmeal/src/main/java/com/backend/birdmeal/entity/UserEntity.java
@@ -1,5 +1,7 @@
 package com.backend.birdmeal.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -29,8 +31,16 @@ public class UserEntity implements Serializable {
     private String userEmail;
 
     @Basic
-    @Column(name="user_authority",length = 20,nullable = false)
-    private String userAuthority;
+    @JsonIgnore
+    // 쓰기 전용 및 조회 불가
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name="user_pass",length = 100)
+    private String userPass;
+
+    @Basic
+    @Column(name="user_role", columnDefinition = "boolean default false")
+    private Boolean userRole;
+
 
     @Basic
     @Column(name="user_nickname",length = 30,nullable = false)
@@ -62,4 +72,10 @@ public class UserEntity implements Serializable {
     @Column(name = "user_update_date", length = 30)
     private String userUpdateDate;
 
+    @ManyToMany // user와 authority 다대다 관계를 일대다, 다대일 관계의 조인테이블로 정의
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "email", referencedColumnName = "user_email")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<AuthorityEntity> authorities;
 }
--- birdmeal/src/main/java/com/backend/birdmeal/service/UserService.java
+++ birdmeal/src/main/java/com/backend/birdmeal/service/UserService.java
@@ -1,11 +1,16 @@
 package com.backend.birdmeal.service;
 
 import com.backend.birdmeal.dto.RegistUserDto;
+import com.backend.birdmeal.dto.ResponseLoginDto;
+import com.backend.birdmeal.entity.AuthorityEntity;
 import com.backend.birdmeal.entity.UserEntity;
+import com.backend.birdmeal.repository.AuthorityRepository;
 import com.backend.birdmeal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -13,25 +18,63 @@ import javax.transaction.Transactional;
 public class UserService {
 
     final UserRepository userRepository;
-
+    final AuthorityRepository authorityRepository;
+    private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
     public boolean signup(RegistUserDto registUserDto){
 
 
         //이메일 주소가 중복이면 false
-        if (userRepository.findByUserEmail(registUserDto.getUserEmail())!= null) {
+        if (userRepository.findByUserEmail(registUserDto.getUserEmail()).isPresent()) {
             return false;
         }
+        boolean role;
+        String pass = registUserDto.getUserEmail().split("@")[0];
+
+        AuthorityEntity authority;
+
+        //권한 확인
+        if(!registUserDto.isUserRole()){
+            role = false;
+            authority = AuthorityEntity.builder()
+                    .authorityName("ROLE_ADMIN")
+                    .build();
+        }
+        else{
+            role = true;
+            authority = AuthorityEntity.builder()
+                    .authorityName("ROLE_CHILD")
+                    .build();
+        }
+
+
+        authorityRepository.save(authority);
 
         UserEntity userEntity = UserEntity.builder()
                 .userSeq(0)
                 .userEmail(registUserDto.getUserEmail())
                 .userNickname(registUserDto.getUserNickname())
-                .userAuthority(registUserDto.getUserAuthority())
+                .userPass(passwordEncoder.encode(pass))
+                .userRole(role)
+                .authorities(Collections.singleton(authority))
                 .build();
 
 
         userRepository.save(userEntity);
 
         return true;
+    }
+
+    public ResponseLoginDto login(String userEmail){
+
+        if (userRepository.findByUserEmail(userEmail)== null) {
+            return null;
+        }
+        ResponseLoginDto responseLoginDto = authService.authorize(userEmail);
+
+        return responseLoginDto;
+
+
+
     }
 }
