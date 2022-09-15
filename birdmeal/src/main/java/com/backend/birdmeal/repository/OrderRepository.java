10eb72f7ad9458adb274161856521178715dddd8
Kim Ji Soo <us13579@naver.com>
2022-09-15 23:19:20 +0900

    [BE] - FEAT : 내 주문 불러오기 (S07P22D101-89)

--- birdmeal/src/main/java/com/backend/birdmeal/repository/OrderRepository.java
+++ birdmeal/src/main/java/com/backend/birdmeal/repository/OrderRepository.java
@@ -3,6 +3,10 @@ package com.backend.birdmeal.repository;
 import com.backend.birdmeal.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     OrderEntity findByOrderSeq(Long orderSeq);
+
+    List<OrderEntity> findAllByUserSeqOrderByOrderDateDesc(long userSeq);
 }
