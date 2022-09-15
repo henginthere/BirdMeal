4ceaa423247108eb87e730033569999b44d9de9a
Kim Ji Soo <us13579@naver.com>
2022-09-15 14:01:06 +0900

    [BE] - FEAT : Seller 등록 상품 목록보기 (S07P22D101-95)

--- birdmeal/src/main/java/com/backend/birdmeal/repository/SellerProductRepository.java
+++ birdmeal/src/main/java/com/backend/birdmeal/repository/SellerProductRepository.java
@@ -3,6 +3,9 @@ package com.backend.birdmeal.repository;
 import com.backend.birdmeal.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SellerProductRepository extends JpaRepository<ProductEntity,Long> {
     ProductEntity findByProductSeq(long productSeq);
+    List<ProductEntity> findAllBySellerSeqOrderByProductCreateDateDesc(long sellerSeq);
 }
