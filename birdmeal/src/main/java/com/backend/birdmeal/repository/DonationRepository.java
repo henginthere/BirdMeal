010b3d958c953d1f48b9c637f7750b1c36c439d3
배혜연 <yeonbb98@naver.com>
2022-09-19 17:59:33 +0900

    [BE] - FEAT : 기부 내역 전체 조회 API (S07P22D101-74)

--- birdmeal/src/main/java/com/backend/birdmeal/repository/DonationRepository.java
+++ birdmeal/src/main/java/com/backend/birdmeal/repository/DonationRepository.java
@@ -1,7 +1,12 @@
 package com.backend.birdmeal.repository;
 
+import com.backend.birdmeal.dto.DonationDto;
 import com.backend.birdmeal.entity.DonationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DonationRepository extends JpaRepository<DonationEntity, Long> {
+
+    List<DonationEntity> findAll();
 }
