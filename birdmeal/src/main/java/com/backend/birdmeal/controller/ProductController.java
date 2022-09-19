ccfc7e8699b8c9fdfee0a3640735cf3ea2165647
배혜연 <yeonbb98@naver.com>
2022-09-19 16:57:23 +0900

    [BE] - FEAT :  카테고리별 상품 목록 요청 API (S07P22D101-71)

--- birdmeal/src/main/java/com/backend/birdmeal/controller/ProductController.java
+++ birdmeal/src/main/java/com/backend/birdmeal/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.backend.birdmeal.controller;
 
 import com.backend.birdmeal.dto.CategoryDto;
+import com.backend.birdmeal.dto.ProductDto;
 import com.backend.birdmeal.service.CategoryService;
 import com.backend.birdmeal.util.ResponseFrame;
 import io.swagger.annotations.Api;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,24 @@ public class ProductController {
         List<CategoryDto> categoryList = categoryService.getCategoryList();
 
         ResponseFrame<List<CategoryDto>> res = ResponseFrame.of(categoryList,"카테고리 목록 요청을 성공했습니다.");
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+
+    /**
+     * 카테고리별 상품 목록 보기
+     *
+     * @param
+     * @return List
+     */
+
+    @ApiOperation(value="카테고리별 상품 목록 보기",response = List.class)
+    @GetMapping("/{category-seq}/list")
+    public ResponseEntity<?> getCategoryProducts(@PathVariable("category-seq") Long categorySeq){
+        List<ProductDto> productList = categoryService.getProductList(categorySeq);
+
+        ResponseFrame<List<ProductDto>> res = ResponseFrame.of(productList,"카테고리별 상품 목록 요청을 성공했습니다.");
 
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
--- birdmeal/src/main/java/com/backend/birdmeal/service/CategoryService.java
+++ birdmeal/src/main/java/com/backend/birdmeal/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.backend.birdmeal.service;
 
 import com.backend.birdmeal.dto.CategoryDto;
+import com.backend.birdmeal.dto.ProductDto;
 import com.backend.birdmeal.entity.CategoryEntity;
 import com.backend.birdmeal.mapper.CategoryMapper;
+import com.backend.birdmeal.mapper.ProductMapper;
 import com.backend.birdmeal.repository.CategoryRepository;
+import com.backend.birdmeal.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +19,18 @@ import java.util.List;
 public class CategoryService {
 
     final CategoryRepository categoryRepository;
+    final ProductRepository productRepository;
 
     public List<CategoryDto> getCategoryList(){
         List<CategoryDto> categoryList = CategoryMapper.MAPPER.toDtoList(categoryRepository.findAll());
 
         return categoryList;
+    }
+
+    public List<ProductDto> getProductList(Long categorySeq){
+        List<ProductDto> productList = ProductMapper.MAPPER.toDtoList(productRepository.findByCategorySeq(categorySeq));
+
+        return productList;
     }
 
 }
