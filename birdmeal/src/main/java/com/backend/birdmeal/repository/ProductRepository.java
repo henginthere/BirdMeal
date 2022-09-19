0eeda2333e30cbe5513f53d5c18768d8c72ab658
henginthere <hypae@naver.com>
2022-09-19 16:57:23 +0900

    [BE] - FEAT :  카테고리별 상품 목록 요청 API (S07P22D101-71)

--- birdmeal/src/main/java/com/backend/birdmeal/repository/ProductRepository.java
+++ birdmeal/src/main/java/com/backend/birdmeal/repository/ProductRepository.java
@@ -3,7 +3,11 @@ package com.backend.birdmeal.repository;
 import com.backend.birdmeal.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     ProductEntity findByProductSeq(long productSeq);
+
+    List<ProductEntity> findByCategorySeq(long categorySeq);
 }
