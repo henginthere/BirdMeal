3327cb843785330d914c1cf2a8daafd0b1c66795
henginthere <hypae@naver.com>
2022-09-19 17:18:00 +0900

    [BE] - FEAT :  상품 상세정보 불러오기 API (S07P22D101-104)

--- birdmeal/src/main/java/com/backend/birdmeal/controller/ProductController.java
+++ birdmeal/src/main/java/com/backend/birdmeal/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.backend.birdmeal.controller;
 import com.backend.birdmeal.dto.CategoryDto;
 import com.backend.birdmeal.dto.ProductDto;
 import com.backend.birdmeal.service.CategoryService;
+import com.backend.birdmeal.service.ProductService;
 import com.backend.birdmeal.util.ResponseFrame;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -23,6 +24,7 @@ import java.util.List;
 public class ProductController {
 
     private final CategoryService categoryService;
+    private final ProductService productService;
 
     /**
      * 카테고리 목록 가져오기
@@ -47,15 +49,39 @@ public class ProductController {
      *
      * @param
      * @return List
-     */
+                */
 
-    @ApiOperation(value="카테고리별 상품 목록 보기",response = List.class)
-    @GetMapping("/{category-seq}/list")
-    public ResponseEntity<?> getCategoryProducts(@PathVariable("category-seq") Long categorySeq){
-        List<ProductDto> productList = categoryService.getProductList(categorySeq);
+        @ApiOperation(value="카테고리별 상품 목록 보기",response = List.class)
+        @GetMapping("/{category-seq}/list")
+        public ResponseEntity<?> getCategoryProducts(@PathVariable("category-seq") Long categorySeq){
+            List<ProductDto> productList = categoryService.getProductList(categorySeq);
 
-        ResponseFrame<List<ProductDto>> res = ResponseFrame.of(productList,"카테고리별 상품 목록 요청을 성공했습니다.");
+            ResponseFrame<List<ProductDto>> res = ResponseFrame.of(productList,"카테고리별 상품 목록 요청을 성공했습니다.");
 
         return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    /**
+     * 상품 상세 정보 불러오기
+     *
+     * @param
+     * @return Object
+     */
+
+    @ApiOperation(value="상품 상세 정보 불러오기",response = Object.class)
+    @GetMapping("/{product-seq}")
+    public ResponseEntity<?> getProductDetail(@PathVariable("product-seq") Long productSeq){
+        ProductDto productDto = productService.getProductDetail(productSeq);
+        ResponseFrame<?> res;
+        if(productDto!=null){
+            res = ResponseFrame.of(productDto,"상품 상세 정보 불러오기를 성공했습니다.");
+        }
+        
+        else {
+            res = ResponseFrame.of(false,"상품 상세 정보 불러오기를 실패했습니다.");
+        }
+        return new ResponseEntity<>(res, HttpStatus.OK);
+
+
     }
 }
