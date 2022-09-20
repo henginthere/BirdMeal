4ba19a1761184cd7d6fafd34f32452a1f802ed80
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-09-20 13:39:34 +0900

    중간저장

--- birdmeal-fe/src/views/ProductRegistView.vue
+++ birdmeal-fe/src/views/ProductRegistView.vue
@@ -1,12 +1,62 @@
 <template>
   <v-app>
-    상품 등록 뷰
+    <h1>상품등록페이지</h1>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="상품명"
+            v-model="name"
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="가격"
+            v-model="price"
+          ></v-text-field>
+        </v-col>
+        <hr/>
+        <!-- <v-btn
+          v-on:click="createTrade(name, Number(price))"
+        > -->
+        <v-btn
+          v-on:click="registProduct"
+        >
+          등록하기
+        </v-btn>
+        {{ca}}
+      </v-row>
+    </v-container>
   </v-app>
 </template>
-
+ 
 <script>
+  import { createTrade } from "@/web3util/events";
   export default {
-    
+    data() {
+      return {
+        name: '',
+        price: 0,
+        ca:''
+      }
+    },
+    methods: {
+      registProduct(){
+        createTrade(this.name, this.price).then(res=>this.ca = res.events.TradeCreated.returnValues.tradeAddress)
+      },
+      createTrade,
+      // check() {
+      //   console.log(this.name,this.price)
+      // }
+    }
   }
 </script>
 
