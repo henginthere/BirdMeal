1ba5d709351b34ce440d43f5b3e6045abe9f465d
dkeka1187 <dkeka1187@naver.com>
2022-09-20 18:02:39 +0900

    [AOS] - STYLE : 마켓 화면 UI 수정 중

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/market/shopping/order/OrderFragment.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/market/shopping/order/OrderFragment.kt
@@ -1,10 +1,15 @@
 package com.ssafy.birdmeal.view.market.shopping.order
 
+import androidx.fragment.app.activityViewModels
 import com.ssafy.birdmeal.R
 import com.ssafy.birdmeal.base.BaseFragment
 import com.ssafy.birdmeal.databinding.FragmentOrderBinding
+import com.ssafy.birdmeal.view.market.shopping.ShoppingViewModel
 
 class OrderFragment : BaseFragment<FragmentOrderBinding>(R.layout.fragment_order) {
+
+    private val shoppingViewModel by activityViewModels<ShoppingViewModel>()
+
     override fun init() {
 
     }
