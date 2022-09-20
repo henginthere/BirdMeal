86f4b9cb776bccd7149c54da78c3a12e81754f20
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-09-20 18:33:48 +0900

    [FE]-FEAT:상품등록,상품목록조회 기능구현

--- birdmeal-fe/src/components/ProductListItem.vue
+++ birdmeal-fe/src/components/ProductListItem.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div v-on:click="goDetail">
+    <img :src="product.productThumbnailImg"/>
+    <br/>
     상품 카테고리: {{category[product.categorySeq]}}
     <br/>
     상품명: {{product.productName}}
     <br/>
-    가격 : {{product.productPrice}}
+    가격 : {{product.productPrice}}ELN
     <v-divider></v-divider>
 
 
@@ -19,6 +21,11 @@
     data() {
       return {
         category:{1:"육류", 2:'채소', 3:"과일", 4:"과자", 5:"빵", 6:"음료"}
+      }
+    },
+    methods: {
+      goDetail() {
+        this.$router.push({name:"product-detail", params:{product:this.product} })
       }
     }
 
