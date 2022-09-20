4475895a9414745ab1fc0e216a62ac118adefc7e
배혜연 <yeonbb98@naver.com>
2022-09-20 11:16:00 +0900

    [BE] - FEAT : S3 이미지 업로드 API (S07P22D101-125,S07P22D101-126)

--- birdmeal/src/main/java/com/backend/birdmeal/BirdmealApplication.java
+++ birdmeal/src/main/java/com/backend/birdmeal/BirdmealApplication.java
@@ -2,6 +2,7 @@ package com.backend.birdmeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -9,8 +10,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaAuditing
 public class BirdmealApplication {
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:secret.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(BirdmealApplication.class, args);
+		new SpringApplicationBuilder(BirdmealApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+		//SpringApplication.run(BirdmealApplication.class, args);
 	}
 
 }
