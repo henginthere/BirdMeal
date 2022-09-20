b29e47915da8372f59ec9be51168bde70c72ebe1
siryeongchoi <siryeongchoi@gmail.com>
2022-09-20 22:46:56 +0900

    [FE] - FEAT : 판매자 auth pinia 적용(S07P22D101-127)

--- birdmeal-fe/src/views/SignupView.vue
+++ birdmeal-fe/src/views/SignupView.vue
@@ -1,56 +1,25 @@
 <template>
   <v-app>
-    <v-btn @click="signup">Login Using Google</v-btn>
+    <v-btn @click="login">Login Using Google</v-btn>
   </v-app>
 </template>
 
-<script>
-import { googleOneTap, decodeCredential } from "vue3-google-login";
-import drf from "@/api/drf.js";
-export default {
-  methods: {
-    signup() {
-      googleOneTap()
-        .then((res) => {
-          // get google oauth user data
-          let credential = res.credential;
-          let userData = decodeCredential(credential);
-          let email = userData.email;
-          let nickname = email.split("@")[0];
+<script setup>
+import { googleOneTap } from 'vue3-google-login';
+import { authState } from '@/stores/auth.js';
 
-          // axios 요청
-          drf
-            .post("/register", {
-              sellerEmail: email,
-              sellerNickname: nickname,
-            })
-            .then(function (res) {
-              // login
-              drf
-                .post("/login", { googleAccessToken: credential })
-                .then((res) => {
-                  if (res.data.success) {
-                    // data state
-                    let data = res.data.data;
-                    let sellerSeq = data.sellerSeq;
-                    let accessToken = data.tokenDto.accessToken;
-                    let refreshToken = data.tokenDto.refreshToken;
-                  }
-                })
-                .catch((error) => {
-                  console.log("login err", error);
-                });
-            })
-            .catch((error) => {
-              console.log("signup error", error);
-            });
-        })
-        .catch((error) => {
-          console.log("Handle the error", error);
-        });
-    },
-  },
-};
+const auth = authState();
+
+function login() {
+  googleOneTap()
+    .then((res) => {
+      auth.signup(res.credential);
+      auth.login(res.credential);
+    })
+    .catch((error) => {
+      console.log('Handle the error', error);
+    });
+}
 </script>
 
 <style lang="scss" scoped></style>
