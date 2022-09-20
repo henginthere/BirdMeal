f84b0cd78a4efb3bc7d2b781fbdf18d690c68986
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-09-20 10:25:12 +0900

    drf수정

--- birdmeal-fe/src/api/drf.js
+++ birdmeal-fe/src/api/drf.js
@@ -1,2 +1,11 @@
-const HOST = "http://127.0.0.1:5173/api/seller"
+import axios from "axios";
+
+// axios 객체 생성
+export default axios.create({
+  baseURL: "https://j7d101.p.ssafy.io/api/seller",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
 
