7f67d3da7dfe84f4c209c6b437f1779234eb9e24
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-20 16:42:22 +0900

    [AOS] - FEAT : 개인키로 지갑 생성 (S07P22D101-55)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/home/CreateWalletDialog.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/home/CreateWalletDialog.kt
@@ -1,6 +1,7 @@
 package com.ssafy.birdmeal.view.home
 
 import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
 import android.content.ClipboardManager
 import android.content.Context
 import android.graphics.Color
@@ -17,11 +18,15 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import com.gun0912.tedpermission.provider.TedPermissionProvider
 import com.ssafy.birdmeal.R
 import com.ssafy.birdmeal.databinding.DialogCreateWalletBinding
 import com.ssafy.birdmeal.utils.*
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.ECKeyPair
 import org.web3j.crypto.WalletUtils
 import java.io.File
+import java.math.BigInteger
 
 class CreateWalletDialog : DialogFragment() {
 
@@ -56,7 +61,8 @@ class CreateWalletDialog : DialogFragment() {
 
         // 개인키 등록하기
         btnRegister.setOnClickListener {
-
+            tvTitle.text = "개인키를 입력해주세요"
+            state = WALLET_PRIVATE
         }
 
         // 지갑 생성
@@ -92,6 +98,45 @@ class CreateWalletDialog : DialogFragment() {
             }
         }
 
+        // 개인키 등록
+        btnCreatePrivate.setOnClickListener {
+            if (validity(etPrivateKey) && validity(etPasswordPrivate)) {
+                val private = etPrivateKey.text.toString()
+                val password = etPasswordPrivate.text.toString()
+                val credentials = Credentials.create(private)
+                val public = credentials.ecKeyPair.publicKey.toString(16)
+
+                Log.d(TAG, "btnCreatePrivate private: ${BigInteger(private, 16)}")
+                Log.d(TAG, "btnCreatePrivate public: $public")
+
+                val path = TedPermissionProvider.context.getWalletPath()
+
+                try {
+                    val walletName = WalletUtils.generateWalletFile(
+                        password,
+                        ECKeyPair(BigInteger(private, 16), BigInteger(public, 16)),
+                        File(path),
+                        false
+                    )
+
+                    setWalletInfo(walletName, password)
+                    userViewModel.setWalletName(walletName)
+                    userViewModel.createCredentials(password)
+
+                } catch (e: Exception) {
+                    Log.d(TAG, "generateWalletFile: $e")
+                    showToast("지갑 생성 실패")
+                }
+            } else {
+                showToast("모두 입력해주세요")
+            }
+        }
+
+        // privateKey 붙여넣기 버튼
+        btnPastePrivate.setOnClickListener {
+            getClipBoardData()
+        }
+
         // privateKey 복사 버튼
         btnCopyPrivateKey.setOnClickListener {
             setClipBoard(tvPrivateKey.text.toString())
@@ -106,6 +151,20 @@ class CreateWalletDialog : DialogFragment() {
         btnClose.setOnClickListener {
             dismiss()
         }
+    }
+
+    // 지갑 생성 후, path, credential 설정
+    private fun setWalletInfo(walletName: String, password: String) {
+        userViewModel.setWalletName(walletName)
+        userViewModel.createCredentials(password)
+
+        val privateKey =
+            userViewModel.credentials.value?.ecKeyPair?.privateKey?.toString(16) ?: ""
+        val eoa = userViewModel.credentials.value?.address.toString()
+        completedWallet(privateKey, eoa)
+
+        showToast("지갑 생성이 완료되었습니다")
+        hideKeyboard()
     }
 
     // 지갑 생성 완료시 호출
@@ -124,6 +183,25 @@ class CreateWalletDialog : DialogFragment() {
         clipboard.setPrimaryClip(clip)
         Log.d(TAG, "setClipBoard: $data")
         showToast("복사가 완료되었습니다")
+    }
+
+    // 클립보드에서 첫번째 데이터 가져오기
+    private fun getClipBoardData() {
+        val clipboard =
+            requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        var pasteData = ""
+
+        if (clipboard.hasPrimaryClip() || clipboard.primaryClipDescription!!.hasMimeType(
+                MIMETYPE_TEXT_PLAIN
+            )
+        ) {
+            val item = clipboard.primaryClip?.getItemAt(0)
+            pasteData = item?.text.toString()
+
+            binding.etPrivateKey.setText(pasteData)
+        }
+        Log.d(TAG, "setClipBoard: $pasteData")
+        showToast("붙여넣기가 완료되었습니다")
     }
 
     // 키보드 내리기
