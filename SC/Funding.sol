012aa778662eaa623514dfc2cd82b150bcbd272b
kunhee <alqp118@naver.com>
2022-09-21 10:15:38 +0900

    [SC]-FEAT:기부컨트랙트 기능추가

--- SC/Funding.sol
+++ SC/Funding.sol
@@ -9,6 +9,11 @@ contract Funding{
     // 엘레나 토큰 컨트랙트
     IERC20 private _currencyContract;
 
+    // 지금까지 결식아동이 인출해간 총금액
+    uint totalWithdrawal;
+
+    mapping (address => uint[]) public donationHistory;
+
     // 코인을 발급한 컨트랙트 주소가 필요하다.
     constructor(address _elenaToken){
         elenaToken = _elenaToken;
@@ -18,6 +23,7 @@ contract Funding{
     // 후원할 코인의 크기를 인자로 받는다
     function funding(uint sendMoney) public payable {
         _currencyContract.transferFrom(msg.sender, address(this), sendMoney*10**18);
+        donationHistory[msg.sender].push(sendMoney);
     }
 
     // 현재 총 기부금을 wei단위로 반환
@@ -26,13 +32,24 @@ contract Funding{
     }
 
     // 아이 계좌의 잔액을 인자로 받는다
-    function takeMoney(uint childBalance) public payable  {
+    function takeMoney(bool isChild) public payable  {
+        require(isChild, 'only chhild can take money');
         // 10만 - 아이 계좌 잔액 만큼 기부금에서 아이계좌로 보낸다.
-        uint validMoney = 100000 - childBalance;
-        _currencyContract.transfer(msg.sender,validMoney*10**18);
+        uint validMoney = (100000*10**18) - (_currencyContract.balanceOf(msg.sender));
+        _currencyContract.transfer(msg.sender,validMoney);
+        totalWithdrawal += validMoney / 10**18;
 
     }
 
+    // 함수호출자의 기부내역
+    function getDonationHistory() public view returns(uint[] memory) {
+        return donationHistory[msg.sender];
+    }
+
+    // 현재까지 아이들이 받아간 기부금 합계 리턴
+    function getTotalWithdrawal() public view returns(uint) {
+        return totalWithdrawal;
+    }
 
 
 }
