fcd1b6b27cfae20260270fa07b456d1bc1cb998d
kimjisoo <us13579@naver.com>
2022-09-21 13:45:03 +0900

    [FE] - FEAT : 판매자 정보 페이지 (S07P22D101-78)

---
+++ birdmeal-fe/src/components/MyPageSellerInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>판매자 정보</h1>
+    <v-card
+  elevation="2"
+ > 
+    
+</v-card>
+</div>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+        }
+      }
+  
+      
+    }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
---
+++ birdmeal-fe/src/views/MyPageUpdateView.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <my-page-seller-info-update></my-page-seller-info-update>
+    </v-app>
+  </template>
+  
+  <script>
+import MyPageSellerInfoUpdateVue from "../components/MyPageSellerInfoUpdate.vue"
+    export default {
+        components: { MyPageSellerInfoUpdateVue }
+      
+    }
+
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
