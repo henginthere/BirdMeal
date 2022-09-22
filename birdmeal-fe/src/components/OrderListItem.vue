6e585514d5cf8960f82a88763bd1a8edaf26a4ec
siryeongchoi <siryeongchoi@gmail.com>
2022-09-22 09:22:08 +0900

    [FE] - FEAT : 주문 목록 페이지 수정, vuetify 컬러 팔렛트 적용

--- birdmeal-fe/src/components/OrderListItem.vue
+++ birdmeal-fe/src/components/OrderListItem.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    오더 리스트 아이템 컴포넌트
-  </div>
+  <v-card class="mx-auto" max-width="344" variant="outlined">
+    <v-card-item>
+      <div>
+        <div class="text-overline mb-1">
+          {{ order.categoryName }}
+        </div>
+        <div class="text-h6 mb-1">
+          {{ order.productName }}
+        </div>
+        <div class="text-caption">상풍입니다.</div>
+      </div>
+    </v-card-item>
+
+    <v-card-actions>
+      <v-btn variant="outlined"> Button </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script setup>
-
+defineProps(['order']);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
