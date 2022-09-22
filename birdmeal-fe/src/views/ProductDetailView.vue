51477356f5961b9d5c26bbe68683ba1f97270c82
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-09-22 17:24:07 +0900

    [FE]-FIX: 상세설명페이지 전체수정 및 TradeContract관련 web3js작성

--- birdmeal-fe/src/views/ProductDetailView.vue
+++ birdmeal-fe/src/views/ProductDetailView.vue
@@ -5,14 +5,14 @@
       <v-row>
         <v-col
           cols="12"
-          sm="6"
-          md="3"
+          sm="2"
+          md="2"
         >
           <v-text-field
             :label="product !== null ? product.productName: 상품명"
             v-model="name"
           ></v-text-field>
-          <v-btn v-on:click="updateProduct">상품명 수정하기</v-btn>
+          <v-btn v-on:click="setName">상품명 수정하기</v-btn>
         </v-col>
         <v-divider />
         <v-col
@@ -24,37 +24,34 @@
             :label="product !== null ? product.productPrice: 상품가격 "
             v-model="price"
           ></v-text-field>
-          <v-btn v-on:click="updateProduct">상품가격 수정하기</v-btn>
+          <v-btn v-on:click="setPrice">상품가격 수정하기</v-btn>
         </v-col>
         <v-divider />
         <hr/>
-        <v-img :src="product !== null ? product.productThumbnailImg: null" max-height="150" max-width="250"/>
+        <v-img :src="product !== null ? productThumbnailImgURL: null" max-height="150" max-width="250"/>
         <form>
           <input type="file" name="productThumbnailImg" id="productThumbnailImg" />
-          <v-btn v-on:click="imgUpload1">섬네일파일 업로드</v-btn>
+          <v-btn v-on:click="imgUpload1">썸네일파일 업로드</v-btn>
+          <v-btn v-on:click="updateproductThumbnailImg">썸네일 수정확정</v-btn>
         </form>
         <v-divider />
         <hr/>
-        <v-img :src="product !== null ? product.productDescriptionImg: null" max-height="150" max-width="250"/>
+        <v-img :src="product !== null ? productDescriptionImgURL: null" max-height="150" max-width="250"/>
         <form>
           <input type="file" name="productDescriptionImg" id="productDescriptionImg" />
-          <v-btn v-on:click="imgUpload2">상세설명파일 업로드</v-btn>
+          <v-btn v-on:click="imgUpload2">상세내용파일 업로드</v-btn>
+          <v-btn v-on:click="updateProductDescriptionImg">상세내용 수정확정</v-btn>
         </form>
         <!-- <v-btn
           v-on:click="createTrade(name, Number(price))"
         > -->
         <v-divider/>
         <v-btn
-          v-on:click="updateProduct"
-        >
-          등록하기
-        </v-btn>
-        <v-btn
           v-on:click="deleteProduct"
         >
           상품 삭제하기
         </v-btn>
-
+        <!-- {{this.product.productCa}} -->
       </v-row>
     </v-container>
 
@@ -62,11 +59,11 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { mapState } from 'pinia';
-import {authState} from '@/stores/auth'
-import http from '../api/http';
-
+  import axios from 'axios'
+  import { mapState } from 'pinia';
+  import {authState} from '@/stores/auth'
+  import http from '../api/http';
+  import {updateName, updatePrice} from "@/web3util/events.js";
   export default {
 
     data() {
@@ -116,12 +113,35 @@ import http from '../api/http';
         }).then(res => this.productDescriptionImgURL = res.data.data)
       },
 
-      updateProduct(){
-        axios.put('https://j7d101.p.ssafy.io/api/seller/product', {
+      setName(){
+        updateName(this.name, this.product.productCa).then(()=>
+        axios.put('https://j7d101.p.ssafy.io/api/seller/product/update/1', {
             productName:this.name,
+            productSeq: this.product.productSeq,
+        },{headers:  {"Content-type": "application/json"}}
+        )).then(() => this.$router.push('/products'))
+      },
+
+      setPrice(){
+        updatePrice(this.price, this.product.productCa).then(()=>
+        axios.put('https://j7d101.p.ssafy.io/api/seller/product/update/2', {
             productPrice: this.price,
             productSeq: this.product.productSeq,
+        },{headers:  {"Content-type": "application/json"}}
+        )).then(() => this.$router.push('/products'))
+      },
+
+      updateproductThumbnailImg(){
+        axios.put('https://j7d101.p.ssafy.io/api/seller/product/update/3', {
+            productSeq: this.product.productSeq,
             productThumbnailImg: this.productThumbnailImgURL,
+        },{headers:  {"Content-type": "application/json"}}
+        ).then(() => this.$router.push('/products'))
+      },
+
+      updateProductDescriptionImg(){
+        axios.put('https://j7d101.p.ssafy.io/api/seller/product/update/4', {
+            productSeq: this.product.productSeq,
             productDescriptionImg: this.productDescriptionImgURL,
         },{headers:  {"Content-type": "application/json"}}
         ).then(() => this.$router.push('/products'))
@@ -131,8 +151,6 @@ import http from '../api/http';
       }
 
     },
-
-
     created(){
       const payload = this.$route.params.productSeq
       this.getProductDetail(payload)
