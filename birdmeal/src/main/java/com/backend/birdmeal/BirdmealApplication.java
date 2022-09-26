26594d1f0825136713d9d177f4955460f6227a61
kimjisoo <us13579@naver.com>
2022-09-26 16:24:14 +0900

    [BE] - FIX : 판매자 정보 수정

--- birdmeal/src/main/java/com/backend/birdmeal/BirdmealApplication.java
+++ birdmeal/src/main/java/com/backend/birdmeal/BirdmealApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class BirdmealApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
