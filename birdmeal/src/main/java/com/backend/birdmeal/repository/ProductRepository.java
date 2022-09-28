1f2f51240003e03b663b3fc763c4509f49c73b8d
kimjisoo <us13579@naver.com>
2022-09-28 14:28:21 +0900

    [BE[ - FIX : 판매자 이름 추가

--- birdmeal/src/main/java/com/backend/birdmeal/repository/ProductRepository.java
+++ birdmeal/src/main/java/com/backend/birdmeal/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.backend.birdmeal.repository;
 
+import com.backend.birdmeal.dto.ProductDto;
 import com.backend.birdmeal.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     ProductEntity findByProductSeq(long productSeq);
-    List<ProductEntity> findByCategorySeq(long categorySeq);
+
+    List<ProductEntity> findAllByCategorySeq(Long categorySeq);
 }
--- birdmeal/src/main/java/com/backend/birdmeal/service/CategoryService.java
+++ birdmeal/src/main/java/com/backend/birdmeal/service/CategoryService.java
@@ -2,14 +2,19 @@ package com.backend.birdmeal.service;
 
 import com.backend.birdmeal.dto.CategoryDto;
 import com.backend.birdmeal.dto.ProductDto;
+import com.backend.birdmeal.dto.ProductResponseDto;
+import com.backend.birdmeal.entity.ProductEntity;
+import com.backend.birdmeal.entity.SellerEntity;
 import com.backend.birdmeal.mapper.CategoryMapper;
 import com.backend.birdmeal.mapper.ProductMapper;
 import com.backend.birdmeal.repository.CategoryRepository;
 import com.backend.birdmeal.repository.ProductRepository;
+import com.backend.birdmeal.repository.SellerInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +24,7 @@ public class CategoryService {
 
     final CategoryRepository categoryRepository;
     final ProductRepository productRepository;
+    final SellerInfoRepository sellerInfoRepository;
 
     public List<CategoryDto> getCategoryList(){
         List<CategoryDto> categoryList = CategoryMapper.MAPPER.toDtoList(categoryRepository.findAll());
@@ -26,10 +32,31 @@ public class CategoryService {
         return categoryList;
     }
 
-    public List<ProductDto> getProductList(Long categorySeq){
-        List<ProductDto> productList = ProductMapper.MAPPER.toDtoList(productRepository.findByCategorySeq(categorySeq));
+    public List<ProductResponseDto> getProductList(Long categorySeq){
+        List<ProductEntity> list = productRepository.findAllByCategorySeq(categorySeq);
 
-        return productList;
+        List<ProductResponseDto> resList = new ArrayList<>();
+
+        for(int i=0; i<list.size(); i++){
+            SellerEntity sellerEntity = sellerInfoRepository.findBySellerSeq(list.get(i).getSellerSeq());
+
+            ProductResponseDto productDto = ProductResponseDto.builder()
+                    .productSeq(list.get(i).getProductSeq())
+                    .categorySeq(list.get(i).getCategorySeq())
+                    .sellerSeq(list.get(i).getSellerSeq())
+                    .sellerName(sellerEntity.getSellerNickname())
+                    .productName(list.get(i).getProductName())
+                    .productPrice(list.get(i).getProductPrice())
+                    .productCa(list.get(i).getProductCa())
+                    .productThumbnailImg(list.get(i).getProductThumbnailImg())
+                    .productDescriptionImg(list.get(i).getProductDescriptionImg())
+                    .productIsDeleted(list.get(i).isProductIsDeleted())
+                    .productCreateDate(list.get(i).getProductCreateDate())
+                    .productUpdateDate(list.get(i).getProductUpdateDate())
+                    .build();
+
+            resList.add(productDto);
+        }
+        return resList;
     }
-
 }
