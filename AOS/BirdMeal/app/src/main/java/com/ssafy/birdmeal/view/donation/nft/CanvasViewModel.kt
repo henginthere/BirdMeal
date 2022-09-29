6d06f6d90a4fd31988ca1b8222440ac7828f2ff7
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-29 10:27:23 +0900

    [AOS] - FEAT: Canvas text 꾸미기 기능 추가 (S07P22D101-102)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/donation/nft/CanvasViewModel.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/donation/nft/CanvasViewModel.kt
@@ -3,13 +3,23 @@ package com.ssafy.birdmeal.view.donation.nft
 import android.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ssafy.birdmeal.utils.SingleLiveEvent
 
 class CanvasViewModel : ViewModel() {
 
     private val _color = MutableLiveData(Color.parseColor("#000000"))
     val color get() = _color
 
+    val text = MutableLiveData<String>()
+
+    private val _textMsgEvent = SingleLiveEvent<Boolean>()
+    val textMsgEvent get() = _textMsgEvent
+
     fun setColor(color: Int) {
         _color.value = color
+    }
+
+    fun emitTextMsg() {
+        _textMsgEvent.postValue(true)
     }
 }
