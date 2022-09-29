97b67a5d57ca715df68bc813cc1daf0e1c0f62e1
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-29 16:47:47 +0900

    [AOS] - FEAT : 포토카드 제작 (S07P22D101-102)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/model/dto/ChildPhotoCardDto.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/model/dto/ChildPhotoCardDto.kt
@@ -4,7 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class ChildPhotoCardDto(
     @SerializedName("userSeq") val userSeq: Int,
-    @SerializedName("nftImg") val nftImg: String,
-    @SerializedName("nftCreateDate") val nftCreateDate: String = "",
-    @SerializedName("nftCnt") val nftCnt: Int,
+    @SerializedName("nftImg") val nftImg: String
 )
