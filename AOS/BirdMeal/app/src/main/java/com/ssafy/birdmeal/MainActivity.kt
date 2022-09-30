c160a7181fc2c5dad514049d19b5f90a9526d5f0
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-30 14:48:09 +0900

    [AOS] - FEAT : 기부자 나의 NFT 목록 보기 (S07P22D101-102)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/MainActivity.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/MainActivity.kt
@@ -24,8 +24,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         initNavigation()
     }
 
-    private fun initNavigation(){
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
         navController = navHostFragment.navController
         binding.bottomNav.apply {
             setupWithNavController(navController)
@@ -33,14 +34,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.homeFragment || destination.id == R.id.createWalletFragment ||
-                destination.id == R.id.donateFragment || destination.id == R.id.canvasFragment){ // 홈, 지갑생성, 기부하기, 포토카드 화면에서 바텀 네비 미표시
-                if(binding.bottomNav.visibility == View.VISIBLE){
+            /*
+            홈, 지갑생성, 기부하기, 포토카드, MY NFT
+            화면에서 바텀 네비 미표시
+             */
+            if (destination.id == R.id.homeFragment || destination.id == R.id.createWalletFragment ||
+                destination.id == R.id.donateFragment || destination.id == R.id.canvasFragment ||
+                destination.id == R.id.myNftListFragment
+            ) {
+                if (binding.bottomNav.visibility == View.VISIBLE) {
                     binding.bottomNav.visibility = View.GONE
                 }
-            }
-            else { // 홈 화면 벗어나면 바텀 네비 표시
-                if(binding.bottomNav.visibility == View.GONE){
+            } else { // 홈 화면 벗어나면 바텀 네비 표시
+                if (binding.bottomNav.visibility == View.GONE) {
                     binding.bottomNav.visibility = View.VISIBLE
                 }
             }
@@ -49,14 +55,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     var waitTime = 0L
     override fun onBackPressed() { // 기부 화면에서 뒤로가기 2번 클릭 시 앱 종료
-        if(navController.currentDestination?.id == R.id.donationFragment){
-            if(System.currentTimeMillis() - waitTime >= 1500){
+        if (navController.currentDestination?.id == R.id.donationFragment) {
+            if (System.currentTimeMillis() - waitTime >= 1500) {
                 waitTime = System.currentTimeMillis()
                 showToast("뒤로가기 버튼을 누르면 종료됩니다.")
+            } else {
+                finish()
             }
-            else { finish() }
+        } else {
+            super.onBackPressed()
         }
-        else { super.onBackPressed() }
     }
 
 }
---
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/my_page/nft/MyNftListAdapter.kt
@@ -0,0 +1,52 @@
+package com.ssafy.birdmeal.view.my_page.nft
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.birdmeal.databinding.ItemMyNftListBinding
+
+class MyNftListAdapter() :
+    ListAdapter<String, MyNftListAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: ItemMyNftListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: String) = with(binding) {
+            imgUrl = data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMyNftListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(
+                oldItem: String,
+                newItem: String
+            ): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+
+            override fun areContentsTheSame(
+                oldItem: String,
+                newItem: String
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+}
