3c369078f6565ab3db7c51ad27d7564d646ff8a3
siryeongchoi <siryeongchoi@gmail.com>
2022-10-01 20:32:04 +0900

    [FE] - STYLE : 주문목록 필터 추가

--- birdmeal-fe/src/views/OrdersView.vue
+++ birdmeal-fe/src/views/OrdersView.vue
@@ -2,7 +2,21 @@
   <v-app>
     <v-container class="text-h4">
       <v-row>
-        <v-col class="ml-4 mt-4 mb-0">주문목록</v-col>
+        <v-col class="ml-4 mt-4 mb-2">주문목록</v-col>
+      </v-row>
+      <v-divider thickness="3" width="600px"></v-divider>
+    </v-container>
+    <v-container class="mb-0 pb-0">
+      <v-row>
+        <v-col lg="2" sm="4" class="mb-0 pb-0">
+          <v-select
+            v-model="filter"
+            :items="filter_list"
+            variant="underlined"
+            label="필터"
+            density="comfortable"
+          ></v-select>
+        </v-col>
       </v-row>
     </v-container>
     <v-container>
@@ -68,6 +82,9 @@ import OrderDetailItem from '@/components/OrderDetailItem.vue';
 const auth = authState();
 
 /** Variable */
+const filter_list = ref(['전체', '배송전', '배송중', '완료']);
+const filter = ref('전체');
+
 let orderList = [];
 
 const currentPage = ref(1);
@@ -85,6 +102,30 @@ onMounted(() => {
   });
 });
 watch(currentPage, updatePageData);
+watch(filter, async function (newVal, oldVal) {
+  http.get(`/order/${auth.user.sellerSeq}`).then(function (res) {
+    orderList = res.data.data;
+  });
+
+  if (newVal == '배송전')
+    orderList = orderList.filter(
+      (item) => !item.orderDeliveryNumber || !item.orderDeliveryCompany
+    );
+
+  if (newVal == '배송중')
+    orderList = orderList.filter(
+      (item) =>
+        item.orderDeliveryNumber &&
+        item.orderDeliveryCompany &&
+        !item.orderToState
+    );
+
+  if (newVal == '완료')
+    orderList = orderList.filter((item) => item.orderToState);
+
+  pageLength.value = Math.ceil(orderList.length / itemsPerPage);
+  updatePageData(1);
+});
 
 /** Function */
 async function updatePageData(pageNum, oldNum) {
