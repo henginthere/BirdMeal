0dd7a5eced29856e917a19e961b725d2d9385002
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-10-03 00:38:37 +0900

    [AOS] - FEAT : 일반인 토큰 충전 로딩 적용 (S07P22D101-151)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/my_page/FillUpMoneyDialog.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/my_page/FillUpMoneyDialog.kt
@@ -1,36 +1,46 @@
 package com.ssafy.birdmeal.view.my_page
 
-import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import com.ssafy.birdmeal.R
 import com.ssafy.birdmeal.databinding.DialogFillUpMoneyBinding
+import com.ssafy.birdmeal.view.loading.LoadingFragmentDialog.Companion.loadingFillUpDialog
 
-class FillUpMoneyDialog(context: Context, private val listener: FillUpMoneyListener): Dialog(context) {
+class FillUpMoneyDialog(context: Context, private val listener: FillUpMoneyListener) :
+    DialogFragment() {
 
-    private lateinit var binding : DialogFillUpMoneyBinding
+    private lateinit var binding: DialogFillUpMoneyBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(
-            LayoutInflater.from(context),
+            inflater,
             R.layout.dialog_fill_up_money,
-            null, false
+            container,
+            false
         )
-        setContentView(binding.root)
+        // 배경 투명하게 바꿔줌
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
 
-        // 배경 투명하게 하기
-        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         initClickListener()
     }
 
-    private fun initClickListener(){
+    private fun initClickListener() {
         binding.apply {
             tvCharge.setOnClickListener {
                 listener.onItemClick(etAmount.text.toString().toInt()) // 수정하려는 수량 보내주기
