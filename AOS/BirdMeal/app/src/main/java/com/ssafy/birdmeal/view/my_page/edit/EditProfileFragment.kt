e7b43adfce380cb8c11cda4c7095d475e4212171
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-10-03 10:18:27 +0900

    [AOS] - FEAT : 로그아웃 회원정보 수정으로 옮김 (S07P22D101-151)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/my_page/edit/EditProfileFragment.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/my_page/edit/EditProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.ssafy.birdmeal.view.my_page.edit
 
+import android.content.Intent
 import android.text.InputFilter
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.ssafy.birdmeal.R
 import com.ssafy.birdmeal.base.BaseFragment
 import com.ssafy.birdmeal.utils.BEIGE
 import com.ssafy.birdmeal.utils.changeStatusBarColor
 import com.ssafy.birdmeal.view.home.UserViewModel
+import com.ssafy.birdmeal.view.login.LoginActivity
 import java.util.regex.Pattern
 
 class EditProfileFragment :
@@ -53,6 +57,20 @@ class EditProfileFragment :
         // 지갑 정보 보기
         btnWallet.setOnClickListener {
             WalletInfoDialog().show(childFragmentManager, "WalletInfoDialog")
+        }
+
+        // 로그아웃
+        btnLogout.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .build()
+            val client = GoogleSignIn.getClient(requireActivity(), gso)
+            client.signOut().addOnCompleteListener {
+                showToast("로그아웃 완료")
+            }
+            Intent(requireContext(), LoginActivity::class.java).apply {
+                startActivity(this)
+                requireActivity().finish()
+            }
         }
     }
 
