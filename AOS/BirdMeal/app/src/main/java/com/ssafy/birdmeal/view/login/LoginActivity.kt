9e4bf09687afaa575398600b0a8fa80b682096e6
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-10-03 10:26:56 +0900

    [AOS] - FEAT : 자동 로그인 (S07P22D101-151)

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/login/LoginActivity.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/login/LoginActivity.kt
@@ -1,12 +1,9 @@
 package com.ssafy.birdmeal.view.login
 
-import android.content.Intent
 import android.content.SharedPreferences
-import com.ssafy.birdmeal.MainActivity
 import com.ssafy.birdmeal.R
 import com.ssafy.birdmeal.base.BaseActivity
 import com.ssafy.birdmeal.databinding.ActivityLoginBinding
-import com.ssafy.birdmeal.utils.JWT
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -17,16 +14,6 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     lateinit var sharedPref: SharedPreferences
 
     override fun init() {
-//        autoLogin()
-    }
-
-    private fun autoLogin() { // 자동 로그인
-        val jwt = sharedPref.getString(JWT, "")
-
-        if (jwt != "") {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
     }
 }
 
