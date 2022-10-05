8ff7fb4f6a206029f6ea65a09462b9e0b1f3f7a9
배혜연 <yeonbb98@naver.com>
2022-10-05 10:50:54 +0900

    [BE] - FEAT : jwt 권한 부여 수정

--- birdmeal/src/main/java/com/backend/birdmeal/config/SecurityConfig.java
+++ birdmeal/src/main/java/com/backend/birdmeal/config/SecurityConfig.java
@@ -81,16 +81,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/user/register").permitAll() // 관리자 권한 허용
                 .antMatchers("/api/seller/**").permitAll() // 관리자 권한
                 .antMatchers("/api/file/**").permitAll() // 파일 업로드
-                .antMatchers("/api/user/**").hasRole("ADMIN")
-                .antMatchers("/api/donation/**").hasRole("ADMIN")
-                .antMatchers("/api/product/**").hasRole("ADMIN")
-                .antMatchers("/api/order/**").hasRole("ADMIN")
-                .antMatchers("/api/nft/**").hasRole("ADMIN")
-                .antMatchers("/api/user/**").hasRole("CHILD")
-                .antMatchers("/api/donation/**").hasRole("CHILD")
-                .antMatchers("/api/product/**").hasRole("CHILD")
-                .antMatchers("/api/order/**").hasRole("CHILD")
-                .antMatchers("/api/nft/**").hasRole("CHILD")
+                .antMatchers("/api/user/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/donation/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/product/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/order/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/nft/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/user/**").hasRole("ROLE_CHILD")
+                .antMatchers("/api/donation/**").hasRole("ROLE_CHILD")
+                .antMatchers("/api/product/**").hasRole("ROLE_CHILD")
+                .antMatchers("/api/order/**").hasRole("ROLE_CHILD")
+                .antMatchers("/api/nft/**").hasRole("ROLE_CHILD")
 
                 .anyRequest().authenticated()
 
