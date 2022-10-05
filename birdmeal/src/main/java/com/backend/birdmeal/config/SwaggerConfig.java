95861b5f511ed024bf0a4021531a85497658db57
henginthere <hypae@naver.com>
2022-10-05 22:59:11 +0900

    [BE] - FIX : swagger

--- birdmeal/src/main/java/com/backend/birdmeal/config/SwaggerConfig.java
+++ birdmeal/src/main/java/com/backend/birdmeal/config/SwaggerConfig.java
@@ -9,9 +9,16 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2 //swagger에 해당하는 어노테이션을 작성한다.
@@ -31,6 +38,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() { //swagger를 연결하기 위한 Bean 작성
         return new Docket(DocumentationType.SWAGGER_2)
+                // swagger에서 jwt 토큰값 넣기위한 설정
+                .securityContexts(Arrays.asList(securityContext()))
+                // swagger에서 jwt 토큰값 넣기위한 설정
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -44,5 +55,28 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .description("backend api document") //알맞는 description을 작성해준다.
                 .version("0.0") //알맞는 버전을 작성해준다.
                 .build();
+    }
+
+    // swagger에서 jwt 토큰값 넣기위한 설정 -> JWT를 인증 헤더로 포함하도록 ApiKey 를 정의.
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    //전역 AuthorizationScope를 사용하여 JWT SecurityContext를 구성.
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope =
+                new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes =
+                new AuthorizationScope[1];
+
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+                new SecurityReference("JWT", authorizationScopes));
     }
 }
