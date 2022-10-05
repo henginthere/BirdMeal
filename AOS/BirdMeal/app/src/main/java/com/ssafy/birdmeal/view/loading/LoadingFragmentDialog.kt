4928b7e03918b18581076255bf61a5144871dc32
DESKTOP-KVCQHCD\multicampus <dkeka1187@naver.com>
2022-10-05 15:54:10 +0900

    [AOS] - STYLE : 오래걸리는 로딩화면에 설명 텍스트 추가2

--- AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/loading/LoadingFragmentDialog.kt
+++ AOS/BirdMeal/app/src/main/java/com/ssafy/birdmeal/view/loading/LoadingFragmentDialog.kt
@@ -8,7 +8,7 @@ class LoadingFragmentDialog {
         val loadingMintingDialog by lazy { LoadingDialog("NFT 받는중...") }
         val loadingFillUpDialog by lazy { LoadingDialog("토큰 충전중...") }
         val loadingLoginDialog by lazy { LoadingDialog("로그인중...") }
-        val loadingDonationDialog by lazy { LoadingDialog("기부중...") }
+        val loadingDonationDialog by lazy { LoadingDialog("기부 중...") }
         val loadingAssumeDialog by lazy { LoadingDialog("구매 확정중...") }
         val loadingOrderDialog by lazy { LoadingDialog("결제 진행중...") }
         val loadingWalletDialog by lazy { LoadingDialog("지갑 생성중...") }
