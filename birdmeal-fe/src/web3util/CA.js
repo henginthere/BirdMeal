8e785e2b395f08bbad60d8a744b6a32960a1afec
kunhee <alqp118@naver.com>
2022-10-06 22:42:28 +0900

    [FE]-FIX:ca갈아끼우기

--- birdmeal-fe/src/web3util/CA.js
+++ birdmeal-fe/src/web3util/CA.js
@@ -1,8 +1,8 @@
 
-export const ElenaTokenContractAddress = "0x24Dae25c6005577E20D6c6927B56564d97C8aBe3";
+export const ElenaTokenContractAddress = "0xbdDbFAbD1Ca3a84a91C8aF34eaE68fE9c9724545";
 
-export const FundingContractAddress = "0x1272AD925859Dd4e485b6d36AD941A5163e380De";
+export const FundingContractAddress = "0x90038Bc80325faafACB52CC3eB19af1D689d732b";
 
-export const ExchangeContractAddress = "0x792bdc2cA619B26B55fe22cC56fe1336A2408dDc";
+export const ExchangeContractAddress = "0x28Fd06c8E713cC32f19d860e4eb07F8A36B2C0ba";
 
-export const TradeManagerContractAddress = "0x5F0A916710f2Cd1fd96D2896C0026B9585C0224f";
+export const TradeManagerContractAddress = "0x008CA1DC0Dc30bbFA38586217161fbcBB7Abed52";
